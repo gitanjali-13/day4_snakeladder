1c4d0027d2acb3924b46dd8cb68e2b93f41513db
gitanjali-13 <gitaanjalide@gmail.com>
2022-09-13 23:10:42 +0530

    Winning position of player

--- src/com/bridgelabz/snake/ladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snake/ladder/SnakeAndLadder.java
@@ -1,9 +1,31 @@
 package com.bridgelabz.snake.ladder;
 
 public class SnakeAndLadder {
+    static int START_POSITION = 0;
+    static int No_play = 0;
+    static int Ladder = 1;
+    static int Snake = 2;
+    static int Player_position = 0;
+
     public static void main(String[] args) {
-        int playerPosition = 0;
-        int afterrolling = (int)Math.floor(Math.random() * 10 ) % 6 + 1;
-        System.out.println("position" +afterrolling);
+        System.out.println("Welcome to Snake and ladder Game");
+        while (Player_position < 100) {
+            int dice = (int) (Math.random() * 10) % 6 + 1;
+            int option = (int) (Math.random() * 10) % 3;
+            if (option == Ladder) {
+                System.out.println("Ladder^^^");
+                Player_position += dice;
+            } else if (option == Snake) {
+                System.out.println("Snake!!!");
+                Player_position += dice;
+
+            } else {
+                System.out.println("NO-PLAY");
+            }
+            if (Player_position < START_POSITION) {
+                Player_position = START_POSITION;
+            }
+            System.out.println("Player position  is :" + Player_position);
+        }
     }
 }
